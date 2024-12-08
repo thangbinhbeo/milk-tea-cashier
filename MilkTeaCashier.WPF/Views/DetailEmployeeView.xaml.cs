0c30656c3cceb6d6eb98de352b917b6458ed96a8
nonglongnhong <long041103@gmail.com>
2024-12-09 00:37:22 +0700

    employee management

---
+++ MilkTeaCashier.WPF/Views/DetailEmployeeView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MilkTeaCashier.Data.Models;
+using System;
+using System.Windows;
+
+namespace MilkTeaCashier.WPF.Views
+{
+ 
+    public partial class DetailEmployeeView : Window
+    {
+        public DetailEmployeeView(Employee employee)
+        {
+            InitializeComponent();
+
+       
+            txtUsername.Text = employee.Username;
+            txtFullName.Text = employee.FullName;
+            txtRole.Text = employee.Role;
+            txtStatus.Text = employee.Status;
+            txtCreatedAt.Text = employee.CreatedAt?.ToString("g"); 
+            txtUpdatedAt.Text = employee.UpdatedAt?.ToString("g"); 
+        }      
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
